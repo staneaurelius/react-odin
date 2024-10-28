bc516bd6e33383c9d705c7b5bbc1c1d9713537a6
staneaurelius <staneaurelius@gmail.com>
2024-10-28 16:09:16 +0700

    Initiate memory card project

---
+++ 2-memory-card/src/components/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import '../style/App.css'
+
+function App() {
+  return (
+    <>
+      
+    </>
+  );
+}
+
+export default App
---
+++ 2-memory-card/src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './style/normalize.css'
+import './style/index.css'
+import App from './components/App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
